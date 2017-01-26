e2da8cc204db30ff8d8e51b06b3669d56b1d10b8
Med <elasri.md@gmail.com>
2017-01-26 22:02:05 +0000

    Delete config.js

--- build/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entry: './assets/js/app.js', // Entries
-  assets_path: 'dist', // Build Path
-  assets_url: '/', // Public Path of Assets
-  filename: 'bundle', // File Name to build
-  hostname: '0.0.0.0', // Server Host Name
-  server_port: 3000, // Server Port
-  client_port: 30000, // Client Port
-}
