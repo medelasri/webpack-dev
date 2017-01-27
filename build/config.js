8d42a0a14753dbc267a18f89ece8d6f93be8092c
Med <elasri.md@gmail.com>
2017-01-27 08:42:34 +0000

    Change config.js

--- build/config.js
+++ build/config.js
@@ -1,25 +1,13 @@
 module.exports = {
     entry: {
-        app: ['./src/assets/js/app.js']
+        app: ['./src/assets/css/app.scss', './src/assets/js/app.js']
     },
     assets_path: 'dist',
     assets_url: '/',
-    filename: 'bundle',
     server_hostname: '0.0.0.0',
-    client_hostname: 'tabs.dev',
+    client_hostname: 'app.dev',
     server_port: 3000,
     client_port: 30000,
-    hot_reload: true,
-    uglifyJs: {
-        sourceMap: true,
-        comments: false,
-        compress: {
-            warnings: false
-        }
-    },
-    css: {
-        minify: true,
-        autoprefixer: true
-    },
+    browsers: ['last 2 versions', 'ie > 8'],
     debug: process.env.NODE_ENV === 'development'
 }
